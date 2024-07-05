1360d65930eaf4a79a2e6756d29161f4cc3bb253
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-07-05 11:22:36 +0700

    feat(layout): integrate AppRouterCacheProvider and CssBaseline in layout.tsx
    - Imported AppRouterCacheProvider from '@mui/material-nextjs/v14-appRouter' and implemented it within layout.tsx,
    - Imported CssBaseline from '@mui/material' and implemented it within layout.tsx,
    - Added ProgressBar component and implemented it in Navbar component,
    - Added Navbar component to and implemented it in RootLayout within layout.tsx,
    
    This integrates AppRouterCacheProvider and CssBaseline from Material-UI in the RootLayout component. Also, includes the Navbar component with a ProgressBar for enhanced user interface.

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import { IoMdClose, IoMdInformationCircleOutline } from 'react-icons/io';
+import ProgressBar from '../ProgressBar/ProgressBar';
+
+const MenuAppBar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          background: 'linear-gradient(101deg,#ffd946,#ffd018 23.96%,#fdbd1a)',
+          boxShadow: 'rgba(0, 0, 0, 0.2) 0px 2px 1px -1px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px'
+        }}
+        elevation={0}
+      >
+        <Toolbar>
+          <IconButton size="large" sx={{ color: 'black' }}>
+            <IoMdClose size={35} />
+          </IconButton>
+          <ProgressBar/>
+          <IconButton sx={{ color: 'black' }}>
+            <IoMdInformationCircleOutline />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default MenuAppBar;
---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 20,
+  borderRadius: 10,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 10,
+    backgroundColor: theme.palette.mode === 'light' ? 'rgb(76, 175, 80)' : '#308fe8',
+  },
+}));
+
+const ProgressBar = () => {
+  return (
+    <Stack spacing={2} sx={{ flexGrow: 1 }}>
+      <BorderLinearProgress variant="determinate" value={20} />
+    </Stack>
+  );
+};
+
+export default ProgressBar;
