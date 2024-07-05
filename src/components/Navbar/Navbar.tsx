0a28e5bf5210796813c936a9b90543a96622b430
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-07-05 15:02:22 +0700

    feat(layout): update Navbar icons
    - Replaced react-icons package with mui/icons-material,
    - Updated icons in Navbar component to use mui/icons-material.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,8 +5,8 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import { IoMdClose, IoMdInformationCircleOutline } from 'react-icons/io';
 import ProgressBar from '../ProgressBar/ProgressBar';
+import { Close, InfoOutlined } from '@mui/icons-material';
 
 const MenuAppBar = () => {
   return (
@@ -17,15 +17,14 @@ const MenuAppBar = () => {
           background: 'linear-gradient(101deg,#ffd946,#ffd018 23.96%,#fdbd1a)',
           boxShadow: 'rgba(0, 0, 0, 0.2) 0px 2px 1px -1px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px'
         }}
-        elevation={0}
       >
         <Toolbar>
           <IconButton size="large" sx={{ color: 'black' }}>
-            <IoMdClose size={35} />
+            <Close fontSize='large'/>
           </IconButton>
           <ProgressBar/>
           <IconButton sx={{ color: 'black' }}>
-            <IoMdInformationCircleOutline />
+            <InfoOutlined />
           </IconButton>
         </Toolbar>
       </AppBar>
