b75c33ad6b4835e80738318f51298c9125417c46
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-07-06 15:11:27 +0700

    refactor: integrate font size slider functionality
    - Implemented font size slider in FontSlider.tsx to dynamically adjust typography size
    - Integrated FontSlider component in MainContent.tsx to control passage text font size
    - Removed unused code lines for improved code cleanliness and maintainability

--- src/app/english-reading-basic-a1/page.tsx
+++ src/app/english-reading-basic-a1/page.tsx
@@ -1,14 +1,12 @@
 import ContentControllerTabs from "@/components/ContentControllerTabs/ContentControllerTabs";
 import MainContent from "@/components/MainContent/MainContent";
 import Navbar from "@/components/Navbar/Navbar";
-import PageContent from "@/components/PageContent/PageContent";
 import { Box } from "@mui/material";
 
 export default function EnglishReadingBasicA1Page() {
   return (
     <Box sx={{ minHeight: '100vh' }}>
       <Navbar />
-      {/* <PageContent /> */}
       <Box sx={{ width: '100%', position: 'fixed', bottom: 0, left: 0, zIndex: 1000}}>
         <MainContent />
         <ContentControllerTabs/>
