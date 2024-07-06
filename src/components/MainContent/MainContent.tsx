26fe2a9d0b1c7451d6d0c49a1ed71336309e84a7
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-07-06 13:37:16 +0700

    feat: Add result display functionality to question handling in MainContent
    - Updated MainContent.tsx to show result section when a user answers a question.
    - Added local state to track selected option, correctness of the answer, and visibility of result section.
    - Implemented handleClickContinue function to reset state and load the next question.
    - Updated question button styles based on the correctness of the answer.

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,12 +1,12 @@
 import { Box, Button, Paper, Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import CustomTabPanel from '../CustomTabPanel/CustomTabPanel';
 import FontSlider from '../Slider/FontSlider';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/utils/redux/store';
-import questions from '@/data/questions.json';
 import { incrementProgressValue } from '@/utils/redux/reducers/progressValueSlice';
 import { incrementQuestionNumber } from '@/utils/redux/reducers/questionNumberSlice';
+import questions from '@/data/questions.json';
 
 type MainContentProps = {
   value: number;
@@ -14,6 +14,13 @@ type MainContentProps = {
 
 const MainContent = ({ value }: MainContentProps) => {
   const dispatch = useDispatch();
+
+  const [state, setState] = useState({
+    selectedOption: '',
+    answerCorrectly: false,
+    showResult: false
+  });
+
   const questionNumber = useSelector((state: RootState) => state.questionNumber.value);
 
   const currentQuestion = questions[questionNumber];
@@ -25,8 +32,15 @@ const MainContent = ({ value }: MainContentProps) => {
   const handleClickOption = (option: string, answer: string) => {
     if(option === answer) {
       dispatch(incrementProgressValue());
+      setState({...state, selectedOption: option, answerCorrectly: true, showResult: true});
+    } else {
+      setState({...state, selectedOption: option, answerCorrectly: false, showResult: true});
     };
+  };
+
+  const handleClickContinue = () => {
     dispatch(incrementQuestionNumber());
+    setState({...state, selectedOption: '', answerCorrectly: false, showResult: false});
   };
 
   return (
@@ -50,13 +64,31 @@ const MainContent = ({ value }: MainContentProps) => {
             </Typography>
           </Paper>
         </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1, px: { xs: 3 , md: 13 }, pt: 2 }}>
-          {currentQuestion.question_data.options.map((option, index) => (
-            <Button key={index} variant='outlined' fullWidth sx={{ minHeight: 55, borderRadius: '10px', display: 'flex', justifyContent: 'start' }} onClick={() => handleClickOption(option, currentQuestion.question_data.answer)}>
-              <Typography fontSize='14px' sx={{ fontWeight: 'bold', textTransform: 'initial' }}>{option}</Typography>
+        {state.showResult ?
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1, px: { xs: 3 , md: 13 }, pt: 2 }}>
+            <Button variant='contained' fullWidth sx={{ bgcolor: state.answerCorrectly ? '#4caf50' : '#f44336', minHeight: 55, borderRadius: '10px', display: 'flex', justifyContent: 'start', ':hover': { bgcolor: state.answerCorrectly ? '#2e7d32' : '#d32f2f' } }}>
+              <Typography fontSize='14px' sx={{ fontWeight: 'bold', textTransform: 'initial' }}>
+                {state.selectedOption}
+              </Typography>
             </Button>
-          ))}
-        </Box>
+            <Paper elevation={1} sx={{ height: 150, p: 1, borderRadius: '10px', mt: '6rem', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Box>
+                <Typography variant='h5' sx={{ color: '#4caf50' }}>{state.answerCorrectly ? 'Excellent!' : 'Try again!'}</Typography>
+              </Box>
+              <Button size='medium' variant='contained' fullWidth sx={{ bgcolor: '#4caf50', py: 1, px: 3, width: 'max-content', fontSize: '14px', textTransform: 'inherit',  borderRadius: '10px', ':hover': { bgcolor: '#2e7d32' } }} onClick={handleClickContinue}>
+                Continue
+              </Button>
+            </Paper>
+          </Box>
+          :
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1, px: { xs: 3 , md: 13 }, pt: 2 }}>
+            {currentQuestion.question_data.options.map((option, index) => (
+              <Button key={index} variant='outlined' fullWidth sx={{ minHeight: 55, borderRadius: '10px', display: 'flex', justifyContent: 'start' }} onClick={() => handleClickOption(option, currentQuestion.question_data.answer)}>
+                <Typography fontSize='14px' sx={{ fontWeight: 'bold', textTransform: 'initial' }}>{option}</Typography>
+              </Button>
+            ))}
+          </Box>
+        }
       </CustomTabPanel>
     </>
   )
