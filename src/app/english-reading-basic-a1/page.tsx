50ae0a28ddcad406a5ba093e6901b68ad80e0e16
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-07-06 08:02:04 +0700

    feat: add new routes and update home page content
    - Added new routes to "/english-reading-basic-a1".
    - Edited the content of the home page.

---
+++ src/app/english-reading-basic-a1/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar/Navbar";
+import PageContent from "@/components/PageContent/PageContent";
+import { Box } from "@mui/material";
+
+export default function EnglishReadingBasicA1Page() {
+  return (
+    <Box sx={{ minHeight: '100vh' }}>
+      <Navbar />
+      <PageContent />
+    </Box>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Navbar from "@/components/Navbar/Navbar";
-import PageContent from "@/components/PageContent/PageContent";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Box sx={{ minHeight: '100vh' }}>
-      <Navbar />
-      <PageContent />
+    <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <Typography variant='h1'>WELCOME</Typography>
+      <Link href="/english-reading-basic-a1" className="px-3 py-2 bg-black text-white rounded-md">English Reading Basic (A1)</Link>
     </Box>
   );
 }
